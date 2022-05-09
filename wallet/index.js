4f4ddcc19bbf2e963bed34592328dee642c7b2f7
Miguel Rivas Perez <mrivasperez@icloud.com>
2022-05-09 08:28:43 -0700

    implemented keypairs

--- wallet/index.js
+++ wallet/index.js
@@ -1,9 +1,11 @@
-const { STARTING_BALANCE } = require("../config");
+const { STARTING_BALANCE } = require("../config.js");
+const { ec } = require("../util");
 
 class Wallet {
     constructor() {
         this.balance = STARTING_BALANCE;
-        
+        const keyPair = ec.genKeyPair(); // generate public/private
+        this.publicKey = keyPair.getPublic(); // create public key
     }
 }
 
