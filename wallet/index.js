81334a77c75d21c4445537cb2cc28c988b99dd19
Miguel Rivas Perez <mrivasperez@icloud.com>
2022-05-31 18:13:07 -0700

    tests and functions to calculate wallet bal from recent transactions

--- wallet/index.js
+++ wallet/index.js
@@ -34,20 +34,30 @@ class Wallet {
     }
 
     static calculateBalance({ chain, address }) {
+        let hasConductedTransaction = false;
+
         let outputsTotal = 0;
 
-        for (let i = 1; i < chain.length; i++) {
+        for (let i = chain.length - 1; i > 0; i--) {
             const currentBlock = chain[i];
             for (let transaction of currentBlock.data) {
+                if (transaction.input.address === address) {
+                    hasConductedTransaction = true;
+                }
+
                 const addressOutput = transaction.outputMap[address];
                 if (addressOutput) {
                     // add to total
                     outputsTotal = outputsTotal + addressOutput;
                 }
             }
+
+            if (hasConductedTransaction) break;
         }
 
-        return STARTING_BALANCE + outputsTotal;
+        return hasConductedTransaction
+            ? outputsTotal
+            : STARTING_BALANCE + outputsTotal;
     }
 }
 
