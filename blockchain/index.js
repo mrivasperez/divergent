73d2675091c87c393aaacd718e1800edc4d36d11
Miguel Rivas Perez <mrivasperez@icloud.com>
2022-06-04 13:22:47 -0700

    finish `validTransactionData()`

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,6 +1,7 @@
 const Block = require("./block");
+const Transaction = require("../wallet/transaction");
 const { cryptoHash } = require("../util");
-const { REWARD_INPUT } = require("../config");
+const { REWARD_INPUT, MINING_REWARD } = require("../config");
 
 class Blockchain {
     constructor() {
@@ -40,7 +41,20 @@ class Blockchain {
                     rewardTransactionCount += 1;
 
                     if (rewardTransactionCount > 1) {
-                        console.error("Miner rewards ezc");
+                        console.error("Miner reward exceeds limit");
+                        return false;
+                    }
+
+                    if (
+                        Object.values(transaction.outputMap)[0] !==
+                        MINING_REWARD
+                    ) {
+                        console.error("Miner reward invalid");
+                        return false;
+                    }
+                } else {
+                    if (!Transaction.validTransaction(transaction)) {
+                        console.error("Invalid transaction");
                         return false;
                     }
                 }
